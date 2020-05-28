601f04b96a138b69ea29790c3933735fdaa6e8f6
Melkam <melkammekonnen899@gmail.com>
2020-05-28 10:48:50 +0300

    Correction

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ console.log(css);
 console.log(color1);
 console.log(color2);
 
-function setGradient(){
+function setGradient() {
     body.style.background = "linear-gradient(to right, "+ color1.value + ", " + color2.value + ")";
 }
 
-color1.addEventListener("input", setGradient());
+color1.addEventListener("input", setGradient);
 
-color2.addEventListener("input", setGradient());
+color2.addEventListener("input", setGradient);
